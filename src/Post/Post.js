269eebd5bc37d907a7f0356bee21a89e6413f9b4
mdriyadmr968 <mdriyadmr968@gmail.com>
2023-02-26 22:15:31 +0600

    added helmet

--- src/Post/Post.js
+++ src/Post/Post.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
+import { Helmet } from "react-helmet-async";
 import { useParams } from "react-router-dom";
-import { FacebookShareButton, FacebookIcon } from "react-share";
+import { FacebookShareButton, TwitterShareButton } from "react-share";
+import { FacebookIcon, TwitterIcon } from "react-share";
 
 const Post = () => {
   const { slug } = useParams();
@@ -23,22 +25,60 @@ const Post = () => {
   });
   return (
     <div>
-      <FacebookShareButton
-        url={"https://syied.netlify.app"}
-        quote={"Check out my custom image!"}
-        hashtag="#customimage"
-      >
-        <FacebookIcon size={32} round={true} />
+      {/* Helmet */}
+      <Helmet>
+        <title>post</title>
         <meta
-          property="og:image"
-          content={"https://i.ibb.co/2gD41VZ/download.jpg"}
+          name="description"
+          content="this is default description from the top of meta tag"
         />
-        <meta property="og:title" content={"Custom Image"} />
+        <link rel="canonical" href="/post" />
+
+        <meta property="og:title" content="this is the title of og tag" />
         <meta
           property="og:description"
-          content={"Check out my custom image!"}
+          content="This is the description of the page"
         />
+        <meta property="og:url" content="https://peing.net/ja/" />
+        <meta property="og:image" content="https://peing.net/images/ogp.png" />
+        <meta property="og:image:alt" content="peing" />
+
+        <meta property="og:locale" content="ja_JP" />
+        <meta property="twitter:title" content="This is the title of twitter" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:image"
+          content="https://peing.net/images/ogp.png"
+        />
+        <meta property="twitter:image:alt" content="peing" />
+        <meta
+          property="twitter:description"
+          content="Peing-質問箱-は、匿名で質問をする＆質問を受け取れるサービスです。質問箱は5秒で作成完了。URLをTwitterやInstagramに投稿して、いろんな人の質問に回答しよう！"
+        />
+        <meta property="twitter:url" content="https://peing.net/ja/" />
+        <meta property="twitter:site" content="@peing_net" />
+        <meta property="twitter:domain" content="peing.net" />
+      </Helmet>
+
+      {/* Facebook & twitter share button  */}
+      <FacebookShareButton
+        url={"https://peing.net/ja/"}
+        quote={"Testing facebook share"}
+        hashtag={"#hashtag"}
+        description={"aiueo"}
+        className="Demo__some-network__share-button"
+      >
+        <FacebookIcon size={32} round /> Facebookでshare
       </FacebookShareButton>
+      <br />
+      <TwitterShareButton
+        title={"test"}
+        url={"https://peing.net/ja/"}
+        hashtags={["hashtag1", "hashtag2"]}
+      >
+        <TwitterIcon size={32} round />
+        Twitterでもshare
+      </TwitterShareButton>
       <a href="/">Home</a>
       <h1 style={{ marginTop: "2%" }}>Title: {post?.title}</h1>
       <h3>Category: {post?.category}</h3>
